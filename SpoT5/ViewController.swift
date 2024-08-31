5551db01d6b0ece520c7d3060ef444bc37432830
V4LM0 <spot001ter@gmail.com>
2024-09-01 00:07:52 +0200

    Initial Commit

---
+++ SpoT5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpoT5
+//
+//  Created by Valentin Majoral on 01/09/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
